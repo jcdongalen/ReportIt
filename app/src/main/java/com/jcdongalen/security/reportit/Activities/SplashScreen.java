95780bdfdf91b5c2dc37a94b688de55fa5aad438
John Carlos Dongalen <johncarlosdongalen@gmail.com>
2019-10-21 02:18:55 +0800

    Migrate to AndroidX and updated gradle versions.

--- app/src/main/java/com/jcdongalen/security/reportit/Activities/SplashScreen.java
+++ app/src/main/java/com/jcdongalen/security/reportit/Activities/SplashScreen.java
@@ -1,12 +1,9 @@
 package com.jcdongalen.security.reportit.Activities;
 
-import android.annotation.SuppressLint;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ProgressBar;
